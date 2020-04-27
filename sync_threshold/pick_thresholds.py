d2fcd4fc7d1356ba2d1570378fd0095c8f79f65f
Shane Loretz <sloretz@openrobotics.org>
2020-04-27 16:32:36 -0700

    Add script to check package counts for ROS 1 buildfarm
    Signed-off-by: Shane Loretz <sloretz@openrobotics.org>

---
+++ sync_threshold/pick_thresholds.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import re
+import requests
+import yaml
+
+
+req = requests.get("https://raw.githubusercontent.com/ros/rosdistro/master/noetic/distribution.yaml")
+if req.status_code != 200:
+    print("Failed to fetch", url)
+else:
+    distro = yaml.safe_load(req.text)
+    count = 0
+    for name, repo in distro['repositories'].items():
+        if 'release' in repo:
+            if 'packages' in repo['release']:
+                count += len(repo['release']['packages'])
+            else:
+                count += 1
+    print(f'released packages: {count}')
+
+
+built_packages = {
+    "focal-amd64": "http://repositories.ros.org/ubuntu/building/dists/focal/main/binary-amd64/Packages",
+    "focal-arm64": "http://repositories.ros.org/ubuntu/building/dists/focal/main/binary-arm64/Packages",
+    "buster-amd64": "http://repositories.ros.org/ubuntu/building/dists/buster/main/binary-amd64/Packages",
+    "buster-arm64": "http://repositories.ros.org/ubuntu/building/dists/buster/main/binary-arm64/Packages",
+}
+
+for name, url in built_packages.items():
+    req = requests.get(url)
+    if req.status_code != 200:
+        print("Failed to fetch", url)
+        continue
+
+    count = 0
+    for line in req.text.split("\n"):
+        if line.startswith("Package: ros-noetic-"):
+            if not line.endswith("-dbgsym"):
+                count += 1
+
+    print(f'{name}: {count} packages built')
