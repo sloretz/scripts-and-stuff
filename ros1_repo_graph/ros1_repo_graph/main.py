4e42f1b6cdd7e7e7e6f325d316af2de90ddc113c
Shane Loretz <sloretz@osrfoundation.org>
2020-05-05 17:44:30 -0700

    Changes for viewing status of Noetic MVP
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ros1_repo_graph/ros1_repo_graph/main.py
+++ ros1_repo_graph/ros1_repo_graph/main.py
@@ -1,15 +1,113 @@
+from .repo_info import releases_repo_info
 from .repo_info import source_entries_repo_info
 from .repo_info import who_is_next
 from .repo_info import who_is_next_vcstool
 from .repo_info import dot_graph
+from .repo_info import filter_repos
+from .repo_info import vcstool
 
 
 def main():
-    print('Hello world')
-    repo_info = source_entries_repo_info(
+    # repo_info = source_entries_repo_info(
+    #     'https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/master/index.yaml',
+    #     'noetic')  # , depth=3)
+    repo_info = releases_repo_info(
         'https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/master/index.yaml',
-        'noetic', depth=3)
+        'noetic')  # , depth=3)
 
-    print(dot_graph(repo_info))
+
+    # Print graph of repos being blocked, ignore repos who aren't blocked or blocking any others
     print(who_is_next(repo_info))
-    print(who_is_next_vcstool(repo_info))
+    # print(who_is_next_vcstool(repo_info))
+
+    # Only care about dot graph to ros-desktop-full for the moment because there's too much other stuff
+    # print(metapackages_info.keys())
+
+    mvp = [
+        'ackermann_msgs',
+        'actionlib',
+        'angles',
+        'bond_core',
+        'catkin',
+        'class_loader',
+        'cmake_modules',
+        'common_msgs',
+        'common_tutorials',
+        'control_msgs',
+        'control_toolbox',
+        'diagnostics',
+        'dynamic_reconfigure',
+        'eigen_stl_containers',
+        'executive_smach',
+        'filters',
+        'gazebo_ros_pkgs',
+        'gencpp',
+        'genlisp',
+        'genmsg',
+        'genpy',
+        'geometric_shapes',
+        'geometry',
+        'geometry_tutorials',
+        'image_common',
+        'image_pipeline',
+        'image_transport_plugins',
+        'interactive_markers',
+        'laser_assembler',
+        'laser_filters',
+        'laser_geometry',
+        'laser_pipeline',
+        'libg2o',
+        'media_export',
+        'message_generation',
+        'message_runtime',
+        'metapackages',
+        'nodelet_core',
+        'object_recognition_msgs',
+        'octomap',
+        'octomap_msgs',
+        'pcl_msgs',
+        'perception_pcl',
+        'pluginlib',
+        'python_qt_binding',
+        'qt_gui_core',
+        'random_numbers',
+        'realtime_tools',
+        'resource_retriever',
+        'robot_state_publisher',
+        'ros',
+        'ros_comm',
+        'ros_comm_msgs',
+        'ros_tutorials',
+        'rosbag_migration_rule',
+        'rosconsole_bridge',
+        'roscpp_core',
+        'roslint',
+        'roslisp',
+        'rospack',
+        'rqt',
+        'rqt_common_plugins',
+        'rqt_robot_plugins',
+        'rviz',
+        'stage',
+        'stage_ros',
+        'std_msgs',
+        'unique_identifier',
+        'urdf_tutorial',
+        'urdfdom_py',
+        'vision_opencv',
+        'visualization_tutorials',
+        'xacro',
+    ]
+
+    print(repr(repo_info['kdl_parser']))
+
+    mvp_info = filter_repos(repo_info, target_repos=mvp)
+
+    unreleased_mvp_info = {}
+    for name, repo in mvp_info.items():
+        if not repo['released']:
+            unreleased_mvp_info[name] = repo
+
+    print(vcstool(unreleased_mvp_info))
+
+    print(dot_graph(unreleased_mvp_info))
--- ros1_repo_graph/ros1_repo_graph/repo_info.py
+++ ros1_repo_graph/ros1_repo_graph/repo_info.py
@@ -1,30 +1,59 @@
+import copy
+
 from ros_buildfarm.status_page import _get_blocked_source_entries_info
+from ros_buildfarm.status_page import _get_blocked_releases_info
 
 
-def filter_repos(repos_info, depth):
+def filter_repos(repos_info, *, depth=None, target_repos=None):
     """Limit how deep tree is, since super deep repos aren't that useful to me."""
     # if depth is 0, just root repo
     # if depth is 1, root repo plus all repos_blocking
 
-    filtered_info = {}
-    while depth >= 0:
-        depth -= 1
-        repos_at_depth = []
-        for name, repo in repos_info.items():
-            if name in filtered_info:
-                # already added this one
-                continue
-            if 0 == len(set(repo['repos_blocked_by']).difference(filtered_info.keys())):
-                # Found repo at this depth
-                repos_at_depth.append(name)
-        for name in repos_at_depth:
-            filtered_info[name] = repos_info[name]
-    return filtered_info
+    ret_info = copy.copy(repos_info)
 
+    if depth is not None:
+        filtered_info = {}
+        while depth >= 0:
+            depth -= 1
+            repos_at_depth = []
+            for name, repo in ret_info.items():
+                if name in filtered_info:
+                    # already added this one
+                    continue
+                if 0 == len(set(repo['repos_blocked_by']).difference(filtered_info.keys())):
+                    # Found repo at this depth
+                    repos_at_depth.append(name)
+            for name in repos_at_depth:
+                filtered_info[name] = ret_info[name]
+        ret_info = filtered_info
+    if target_repos is not None:
+        filtered_info = {}
+        for name, repo in ret_info.items():
+            include = False
+            if 'recursive_repos_blocking' in repo:
+                for r in repo['recursive_repos_blocking']:
+                    if r in target_repos:
+                        include = True
+                        break
+            if name in target_repos:
+                include = True
+            if include:
+                filtered_info[name] = repo
+        ret_info = filtered_info
+
+    return ret_info
 
 
 def source_entries_repo_info(config_url, ros_distro_name, depth=None):
     repos_info = _get_blocked_source_entries_info(config_url, ros_distro_name)
+
+    if depth:
+        repos_info = filter_repos(repos_info, depth)
+    return repos_info
+
+
+def releases_repo_info(config_url, ros_distro_name, depth=None):
+    repos_info = _get_blocked_releases_info(config_url, ros_distro_name)
 
     if depth:
         repos_info = filter_repos(repos_info, depth)
@@ -48,9 +77,10 @@ def dot_graph(repos_info):
         for blocked_by in repo['repos_blocked_by']:
             if blocked_by in repos_info:
                 edges.add((name, blocked_by))
-        for blocking in repo['repos_blocking']:
-            if blocking in repos_info:
-                edges.add((blocking, name))
+        if 'repos_blocking' in repo:
+            for blocking in repo['repos_blocking']:
+                if blocking in repos_info:
+                    edges.add((blocking, name))
 
     edges_dot = []
     nodes_dot = []
@@ -65,22 +95,39 @@ def dot_graph(repos_info):
         nodes='\n'.join(nodes_dot))
 
 
-def who_is_next_info(repos_info):
+def num_repos_blocking(repo):
+    if 'recursive_repos_blocking' in repo:
+        return len(repo['recursive_repos_blocking'])
+    elif 'repos_blocking' in repo:
+        return len(repo['repos_blocking'])
+    return 0
+
+
+def who_is_next_info(repos_info, ignore_repos=('orocos_kinematics_dynamics', 'bfl')):
     next_repos = {}
 
     for name, repo in repos_info.items():
+        if name in ignore_repos:
+            continue
         if repo['released']:
             continue
         if repo['repos_blocked_by']:
-            continue
+            do_continue = False
+            for br in repo['repos_blocked_by'].keys():
+                if br not in ignore_repos:
+                    do_continue = True
+                    break
+            if do_continue:
+                continue
         next_repos[name] = repo
 
     report = []
-    for name, repo in sorted(next_repos.items(), key=lambda t: len(t[1]['repos_blocking']), reverse=True):
+
+    for name, repo in sorted(next_repos.items(), key=lambda t: num_repos_blocking(t[1]), reverse=True):
         report.append({
                 'name': name,
-                'blocking': len(repo['repos_blocking']),
-                'url': repo['url']
+                'blocking': num_repos_blocking(repo),
+                'url': repo['url'] if 'url' in repo else None
             })
     return report
 
@@ -91,24 +138,23 @@ def who_is_next(repos_info):
     report = []
     for info in report_info:
         report.append("{name} (blocking {amt})".format(name=info['name'], amt=info['blocking']))
-        report.append("\t{url}".format(url=info['url']))
     return "\n".join(report)
 
 
 def who_is_next_vcstool(repos_info):
-    next_repos = {}
+    return vcstool(who_is_next_info(repos_info))
+
+
+def vcstool(repos_info):
+    repos_file = ['repositories:']
 
     for name, repo in repos_info.items():
-        if repo['released']:
-            continue
-        if repo['repos_blocked_by']:
-            continue
-        next_repos[name] = repo
-
-    repos_file = ['repositories:']
-    for name, repo in sorted(next_repos.items(), key=lambda t: len(t[1]['repos_blocking']), reverse=True):
-        if repo['url']:
+        if 'url' in repo and repo['url']:
             repos_file.append("""  {name}:
     type: git
     url: {url}""".format(name=name, url=repo['url']))
+        else:
+            repos_file.append("""#  {name}:
+#    type: git
+#    url: {url}""".format(name=name, url=None))
     return "\n".join(repos_file)
