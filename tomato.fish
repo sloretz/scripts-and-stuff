f496e125393819b1537b542f26648e7b25238496
Shane Loretz <sloretz@osrfoundation.org>
2020-08-05 13:49:41 -0700

    Add Tomato timer
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

---
+++ tomato.fish
@@ -0,0 +1,58 @@
+#!/usr/bin/env fish
+
+echo 25 minutes to go
+sleep 60
+echo 24 minutes to go
+sleep 60
+echo 23 minutes to go
+sleep 60
+echo 22 minutes to go
+sleep 60
+echo 21 minutes to go
+sleep 60
+echo 20 minutes to go
+sleep 60
+echo 19 minutes to go
+sleep 60
+echo 18 minutes to go
+sleep 60
+echo 17 minutes to go
+sleep 60
+echo 16 minutes to go
+sleep 60
+echo 15 minutes to go
+sleep 60
+echo 14 minutes to go
+sleep 60
+echo 13 minutes to go
+sleep 60
+echo 12 minutes to go
+sleep 60
+echo 11 minutes to go
+sleep 60
+echo 10 minutes to go
+sleep 60
+echo 9 minutes to go
+sleep 60
+echo 8 minutes to go
+sleep 60
+echo 7 minutes to go
+sleep 60
+echo 6 minutes to go
+sleep 60
+echo 5 minutes to go
+sleep 60
+echo 4 minutes to go
+sleep 60
+echo 3 minutes to go
+sleep 60
+echo 2 minutes to go
+sleep 60
+echo 1 minutes to go
+sleep 60
+echo You did it!
+
+while true
+  notify-send -a "Tomato timer" "Hello" "Time for a 5 minute break!"
+  sleep 15
+end
